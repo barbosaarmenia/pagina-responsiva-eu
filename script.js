f2e6a10c403c52cfec03c25bb0b7e10f714a3efb
armenia <barbosaarmenia@gmail.com>
2025-01-13 12:37:08 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+class NavBar{
+    constructor(MobileMenu,NavLists,NavLinks){
+        this.MobileMenu=document.querySelector(MobileMenu);
+        this.NavLists=document.querySelector(NavLists);
+        this.NavLinks=document.querySelector(NavLinks);
+        this.activeClass="active";
+        this.handleClick=this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        console.log(this);
+        this.NavLists.classList.toggle(this.activeClass);
+    }
+    addClickEvent(){
+        this.MobileMenu.addEventListener("click",this.handleClick);
+    }
+    init(){
+        if(this.MobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+let dado=new NavBar(".mobile-menu",".nav-list",".nav-list li");
+dado.addClickEvent();
+
+const painels = document.querySelectorAll(".painel")
+
+painels.forEach((projeto) => {
+    projeto.addEventListener("click", () =>{
+        removerActivateClasses();
+        projeto.classList.add("active");
+    })
+})
+
+function removerActivateClasses(){
+    painels.forEach(painel => {
+        painel.classList.remove("active");
+    })
+}
